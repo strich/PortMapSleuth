eed3b04e33668fd286807cc5cd2142a6fa1eef78
Scott Richmond <s.t.richmond@gmail.com>
2014-09-13 15:17:34 +1000

    Added code to help daemonize the server on Unix.

--- PortMapSleuth/Program.cs
+++ PortMapSleuth/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Funq;
 using ServiceStack;
+using Mono.Unix;
+using Mono.Unix.Native;
 
 namespace PortMapSleuth {
     internal class Program {
@@ -12,7 +14,22 @@ namespace PortMapSleuth {
 
             Console.WriteLine("AppHost Created at {0}, listening on {1}", DateTime.Now, listeningOn);
 
-            Console.ReadKey();
+            // ------------------------------------------------------------- //
+            // The below code is to support Unix daemonization.
+            // Remove this and the Mono requirement to run it on Windows
+            UnixSignal[] signals = new[] { 
+                new UnixSignal(Signum.SIGINT), 
+                new UnixSignal(Signum.SIGTERM), 
+            };
+
+            // Wait for a unix signal:
+            for (bool exit = false; !exit; ) {
+                int id = UnixSignal.WaitAny(signals);
+                if (id >= 0 && id < signals.Length)
+                    if (signals[id].IsSet)
+                        exit = true;
+            }
+            // ------------------------------------------------------------- //
         }
 
         //Web Service Host Configuration
